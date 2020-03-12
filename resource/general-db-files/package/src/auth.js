908d3c5e6b3fd7c4debacfd542f1651530d9840f
fox666 <fox.jiang@zoom.us>
2020-03-12 15:38:17 +0800

    change auth code

--- resource/general-db-files/package/src/auth.js
+++ resource/general-db-files/package/src/auth.js
@@ -1,15 +1,14 @@
-let jwt = require('jsonwebtoken');
 let moment = require('moment');
 module.exports = async (req, res) => {
   let { zoomApp, zoomError, databaseModels,request } = res.locals;
   if (!zoomError) {
     let tokens = zoomApp.auth.getTokens(); //get zoom token
-    let info = jwt.decode(zoomApp.auth.tokens.access_token); //parse token to multiple information
     if (databaseModels) {
       let time = moment().add(tokens.expires_in, 'seconds');
       try {
+        let userInfo = await zoomApp.request({url:'/v2/users/me', method:'get'});
         await databaseModels.zoom.save({
-          zoom_account_id: info.accountId,
+          zoom_account_id: userInfo.account_id,
           access_token: tokens.access_token,
           refresh_token: tokens.refresh_token,
           expires_date: time.format()
@@ -17,18 +16,8 @@ module.exports = async (req, res) => {
         res.render('index');
       } catch (e) {
         res.send('error');
-        // console.log(e);
       }
     }
-    /* 
-      UserInfo:{
-        clientId:string;
-        code:string;
-        userId:string;
-        accountId:string;
-        [propName: string]:string | number;  
-      }
-    */
   } else {
     res.send('error');
   }
--- resource/serverless/package/src/auth.js
+++ resource/serverless/package/src/auth.js
@@ -1,20 +1,6 @@
-// let jwt = require('jsonwebtoken');
-
 module.exports = async (req, res) => {
   let { zoomApp, zoomError,databaseModels,request} = res.locals;
-  if (!zoomError) {
-    // let tokens = zoomApp.auth.getTokens(); //get zoom token
-    // let userInfo = jwt.decode(zoomApp.auth.tokens.access_token); //parse token to multiple information
-    /* 
-      UserInfo:{
-        clientId:string;
-        code:string;
-        userId:string;
-        accountId:string;
-        [propName: string]:string | number;  
-      }
-    */
-    
+  if (!zoomError) {    
     res.render('index');
   }
   else{
